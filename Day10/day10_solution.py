05f5569035b6cf6a6076a8b61d7bea0feb743367
Rachel <=>
2021-12-10 21:18:07 +0000

    Two working parts to day 10

--- Day10/day10_solution.py
+++ Day10/day10_solution.py
@@ -1,14 +1,17 @@
+# Module
+import statistics
+
 # test input
-input1 = ["[({(<(())[]>[[{[]{<()<>>",
-          "[(()[<>])]({[<{<<[]>>(",
-          "{([(<{}[<>[]}>{[]{[(<()>",
-          "(((({<>}<{<{<>}{[]{[]{}",
-          "[[<[([]))<([[{}[[()]]]",
-          "[{[{({}]{}}([{[{{{}}([]",
-          "{<[[]]>}<{[{[{[]{()[[[]",
-          "[<(<(<(<{}))><([]([]()",
-          "<{([([[(<>()){}]>(<<{{",
-          "<{([{{}}[<[[[<>{}]]]>[]]"]
+# input1 = ["[({(<(())[]>[[{[]{<()<>>",
+#           "[(()[<>])]({[<{<<[]>>(",
+#           "{([(<{}[<>[]}>{[]{[(<()>",
+#           "(((({<>}<{<{<>}{[]{[]{}",
+#           "[[<[([]))<([[{}[[()]]]",
+#           "[{[{({}]{}}([{[{{{}}([]",
+#           "{<[[]]>}<{[{[{[]{()[[[]",
+#           "[<(<(<(<{}))><([]([]()",
+#           "<{([([[(<>()){}]>(<<{{",
+#           "<{([{{}}[<[[[<>{}]]]>[]]"]
 
 # Real input
 input1 = []
@@ -16,21 +19,24 @@ with open("input.txt") as file:
     for x in file.readlines():
         input1.append(x.split("\n")[0])  # Read in input line by line, stripping newline character off end
 
-ans_dict = {")": 0,
-            "]": 0,
-            "}": 0,
-            ">": 0}
-
+# Record what data we're allowed
 valid_openings = ["(", "[", "{", "<"]
 valid_endings = [")", "]", "}", ">"]
 valid_parings = {"(": ")",
                  "[": "]",
                  "{": "}",
                  "<": ">"}
+
+# Part 1
 output_scores = {")": 3,
                  "]": 57,
                  "}": 1197,
                  ">": 25137}
+
+ans_dict = {")": 0,
+            "]": 0,
+            "}": 0,
+            ">": 0}
 
 for x in input1:
     temp = []
@@ -57,15 +63,40 @@ for ending in valid_endings:
 print(ans1)
 
 
-#     x0 = x[0]
-#     xn = x[-1]
-#     print([x0, xn, ord(x0), ord(xn)])
-#     if x0 == "(":
-#         if xn != ")" and xn in valid_endings:
-#             ans_dict[xn] += 1
-#     else:
-#         if ord(x0) + 2 != ord(xn) and xn in valid_endings: # Use that other brackets are two different in ASCII
-#             ans_dict[xn] += 1
-#
-# print(ans_dict)
+# Part 2
+scores = []
 
+output_scores2 = {")": 1,
+                  "]": 2,
+                  "}": 3,
+                  ">": 4}
+
+for x in input1:
+    temp = []
+    string_len = len(x)
+    for y in range(string_len):
+        my_char = x[y]
+        if my_char in valid_openings:
+            temp.append(x[y])
+        elif my_char in valid_endings:
+            most_recent = temp[-1]
+            # print([most_recent, my_char])
+            if valid_parings[most_recent] != my_char:
+                break  # Move onto next item in outer loop
+            temp.pop()  # Remove last element
+        if y == string_len - 1 and len(temp) != 0:  # If we get here the string is incomplete
+            print(temp)
+            score = 0
+            for item in reversed(temp):
+                print(item)
+                item_pair = valid_parings[item]
+                score *= 5
+                score += output_scores2[item_pair]
+            print(score)
+            scores.append(score)
+
+print(scores)
+
+# Want the middle one which is the median
+ans2 = statistics.median(scores)
+print(ans2)
